39b45b5c1421024ec161d5c76e1d251e199c0c3e
Maniyuvan <maniyuvan446@gmail.com>
2023-04-13 16:08:46 +0530

    moved mongo uri to env

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ module.exports = {
     connectDB: async(app) => {
         try {
             // let mongoUrl = `mongodb://${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/${process.env.MONGO_DB_NAME}`;
-            let mongoUrl = 'mongodb+srv://wl_user:Admin_123@cluster0.zktz49w.mongodb.net/?retryWrites=true&w=majority';
+            let mongoUrl = process.env.MONGO_URI;
             return await mongoose.connect(mongoUrl);
         } catch (error) {
             throw error;
