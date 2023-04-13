27e027b818c3d71e0222e0415a20f7c6253aa05a
Maniyuvan <maniyuvan446@gmail.com>
2023-04-13 17:12:53 +0530

    fix: added cors

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const morgan = require('morgan');
 function createServer() {
 	const app = express();
     app.use(express.json());
-    // app.use(cors());
+    app.use(cors());
     // app.use((req,res,next) => {
     //     res.header("Access-Control-Allow-Origin","*");
     //     res.header("Access-Control-Allow-Methods","GET,POST,OPTIONS");
